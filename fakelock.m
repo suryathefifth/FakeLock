491ac00b5422fbf0c94ee0df9a4e4fd91b16f5bc
Surya Vezhavendan <67297811+suryathefifth@users.noreply.github.com>
2020-10-26 16:46:43 -0400

    Add background image capability for one screen

--- fakelock.m
+++ fakelock.m
@@ -12,6 +12,34 @@ uipanel(fakelockFig, 'units', 'norm', 'pos', [0, 0, 1, 1], 'backgroundcolor', [0
 
 
 
+oasisFig = figure('MenuBar', 'none', 'windowstate', 'fullscreen', 'tag', 'oasisFig');
+oasisImageDisplay =  uipanel(oasisFig, 'backgroundcolor', [0.3 0 0], 'units', 'norm', 'pos', [0, 0, 1, 1], 'bordertype', 'none', 'Tag', 'oasisImageDisplay');
 
+
+
+[oasisImg, ~, ~] = imread('oasisJPG.jpg');
+% [oasisImg, ~, ~] = imread('centauri-logo.png');
+
+oasisAx = axes(oasisImageDisplay, 'Units','norm');
+oasisImg = flip(oasisImg, 1);
+
+pause(1) %the axes wont expand for some reason without this pause!!!
+oasisAx.InnerPosition = [0, 0, 1, 1]; %eliminate the margins for labels
+
+pause(1) %the axes wont expand for some reason without this pause!!!
+oasisAx.Position = [0, 0, 1, 1]; %eliminate the margins for labels
+
+oasisAx.Units = 'pixels';
+% resizePos = get(oasisAx,'Position');
+% oasisImg= imresize(oasisImg, [resizePos(3) resizePos(3)]); %idk why its not (3) and (4)... something with the aspectratio
+% oasisImg= imresize(oasisImg, [2000 1200]);
+
+% pause(1) %the axes wont expand for some reason without this pause!!!
+image(oasisAx, 'CData', imresize(oasisImg, [1999 1999])); %for some reason this is full screen on my monitor... this number can't be larger than monitor btw. and i know my monitor isnt 1999x1999 (ITS NOT A SQUARE) so maybe it has to do with fullscreen tag?
+oasisAx.Visible = 'off';
+% oasisAx.DataAspectRatioMode = 'manual';
+% oasisAx.DataAspectRatio = [1 2 1];
+
+% oasisAx.Clipping = 'off';
 
 
